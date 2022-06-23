d09151d80bde9e07314370a0330cc2c70a3ad347
Celestin Niyonsaba <celestin@Celestins-MacBook-Pro.local>
2022-06-23 23:28:20 +0200

    add main page and bottom navigation Bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:travel_app/pages/navpages/main_page.dart';
 import 'package:travel_app/pages/welcome_page.dart';
 
 void main() {
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const WelcomePage(),
+      home: const MainPage(),
     );
   }
 }
---
+++ lib/pages/navpages/main_page.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:travel_app/pages/navpages/bar_item_page.dart';
+import 'package:travel_app/pages/home_page.dart';
+import 'package:travel_app/pages/navpages/my_page.dart';
+import 'package:travel_app/pages/navpages/search_page.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  List pages = [HomePage(), BarItemPage(), SearchPage(), MyPage()];
+
+  int currentIndex = 0;
+  void onTap(int index) {
+    setState(() {
+      currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: pages[currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        unselectedFontSize: 0,
+        selectedFontSize: 0,
+          type: BottomNavigationBarType.fixed,
+          backgroundColor: Colors.white,
+          onTap: onTap,
+          currentIndex: currentIndex,
+          selectedItemColor: Colors.black54,
+          unselectedItemColor: Colors.grey.withOpacity(0.5),
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          elevation: 0,
+          items: const [
+            BottomNavigationBarItem(label: 'Home', icon: Icon(Icons.apps)),
+            BottomNavigationBarItem(label: 'Bar', icon: Icon(Icons.bar_chart)),
+            BottomNavigationBarItem(label: 'Search', icon: Icon(Icons.search)),
+            BottomNavigationBarItem(label: 'My', icon: Icon(Icons.person)),
+          ]),
+    );
+  }
+}
